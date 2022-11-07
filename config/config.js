9096c9bcf94c6603ce4a8ab03a55d0b1bbd6e77c
Alexander Lazarenko <ferrosful@gmail.com>
2022-11-07 09:32:15 +0200

    Updated connection string

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 const databaseName = 'online-shop';
-const mongoDBConnString = 'mongodb://127.0.0.1:27017';
+const mongoDBConnString = 'mongodb://0.0.0.0:27017/';
 
 module.exports = {
     databaseName, mongoDBConnString
